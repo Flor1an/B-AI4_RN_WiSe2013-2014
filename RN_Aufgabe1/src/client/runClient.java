a57bfc202ba62d0c2647b1a80e24445233ab30dd
Florian <Florian@Flo-UB.fritz.box>
2013-10-19 21:11:25 +0200

    minor

--- RN_Aufgabe1/src/client/runClient.java
+++ RN_Aufgabe1/src/client/runClient.java
@@ -4,7 +4,7 @@ import helper.UserData;
 
 public class runClient {
 	public static void main(String[] args) {
-		UserData user1 = new UserData("flbjkl@gmx.de","simplepass", "pop.gmx.net", 110,false);
+		UserData user1 = new UserData("flbjkl@gmx.de","pw", "pop.gmx.net", 110,false);
 		
 		Pop3Client c1 = new Pop3Client(user1);
 		
